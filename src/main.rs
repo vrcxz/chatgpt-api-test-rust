2f52138cf255c3c576778be710ef027a0a53dce2
Von Ryan Nogadas <74548381+vonnogadas@users.noreply.github.com>
2023-02-23 17:30:16 +0800

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use reqwest::{Client, header::{USER_AGENT, HeaderValue}};
+use serde_json::json;
+use std::error::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    choices: Choices,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Choices {
+    text: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    
+    let response = Client::new()
+        .post("https://api.openai.com/v1/engines/davinci-codex/completions")
+        .header(USER_AGENT, HeaderValue::from_static("reqwest"))
+        .header("Content-Type", HeaderValue::from_static("application/json"))
+        .bearer_auth("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")
+        .json(&json!({
+            "prompt": "Tell me a story about some fox.",
+            "temperature": 0.5,
+            "max_tokens": 100
+        }))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+       
+    println!("{}", response);
+ 
+    Ok(())
+}
